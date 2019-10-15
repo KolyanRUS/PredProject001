8ced21c53608edd0037328d6dbea18f6db508e4c
Nikolay Klimov <krovoron@outlook.com>
2019-10-15 17:04:34 +0300

    COMMIT SPRING SECURITY 15.10.2019__17-04

--- src/main/java/com/javamaster/securityConfig/CustomizeAuthenticationSuccessHandler.java
+++ src/main/java/com/javamaster/securityConfig/CustomizeAuthenticationSuccessHandler.java
@@ -1,12 +1,15 @@
 package com.javamaster.securityConfig;
 
 import java.io.IOException;
+import java.util.Collection;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.javamaster.model.Role;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 @Component
@@ -16,7 +19,10 @@ public class CustomizeAuthenticationSuccessHandler implements AuthenticationSucc
                                         HttpServletResponse response, Authentication authentication)
             throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_OK);
-        if(authentication.getAuthorities().contains("ROLE_ADMIN")){
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Role r = (Role) authorities.toArray()[0];
+        boolean admin = "ROLE_ADMIN".equals(r.getRole());
+        if(admin){
             response.sendRedirect("/admin");
         } else {
             response.sendRedirect("/user");
